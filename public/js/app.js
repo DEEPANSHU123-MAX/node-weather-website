3d764769b5139f8dd6eba7ed559cc2d54fb1c0e1
deepanshu123 <deepanshu123tyagi@gmail.com>
2021-05-12 14:58:54 +0530

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -26,7 +26,7 @@ messagetwo.textContent ='Loading...'
 messageone.textContent =''
 
     const location = search.value
-    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+    fetch('/weather?address='+ location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             messagetwo.textContent = data.error
